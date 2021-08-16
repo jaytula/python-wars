336368ec5622d9a131f89c3fca76bcabc445ceef
Jay T <jaytula@gmail.com>
2021-08-16 16:08:27 -0700

    Finish 'You're a square'

---
+++ youre_a_square.py
@@ -0,0 +1,15 @@
+def is_square(n: int):    
+    return n ** (0.5) % 1 == 0 if n >= 0 else False
+
+import codewars_test as test
+
+@test.describe("Fixed Tests")
+def fixed_tests():
+    @test.it('Basic Test Cases')
+    def basic_test_cases():
+        test.assert_equals(is_square(-1), False, "-1: Negative numbers cannot be square numbers")
+        test.assert_equals(is_square( 0), True, "0 is a square number (0 * 0)")
+        test.assert_equals(is_square( 3), False, "3 is not a square number")
+        test.assert_equals(is_square( 4), True, "4 is a square number (2 * 2)")
+        test.assert_equals(is_square(25), True, "25 is a square number (5 * 5)")
+        test.assert_equals(is_square(26), False, "26 is not a square number")
