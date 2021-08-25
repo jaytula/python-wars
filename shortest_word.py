f7b710e957eb280604613222f5cfd8d021b3c0d1
Jay T <jaytula@gmail.com>
2021-08-24 17:31:02 -0700

    Finish 'Shortest Word'

---
+++ shortest_word.py
@@ -0,0 +1,16 @@
+def find_short(s: str):
+    return min([len(word) for word in s.split(' ')])
+    
+
+import codewars_test as test
+
+@test.describe("Fixed Tests")
+def fixed_tests():
+    @test.it('Basic Test Cases')
+    def basic_test_cases():
+        test.assert_equals(find_short("bitcoin take over the world maybe who knows perhaps"), 3)
+        test.assert_equals(find_short("turns out random test cases are easier than writing out basic ones"), 3)
+        test.assert_equals(find_short("lets talk about javascript the best language"), 3)
+        test.assert_equals(find_short("i want to travel the world writing code one day"), 1)
+        test.assert_equals(find_short("Lets all go on holiday somewhere very cold"), 2)   
+        test.assert_equals(find_short("Let's travel abroad shall we"), 2)
